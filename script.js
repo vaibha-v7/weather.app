78e0bf25325b75513bab1b0ada9a6168521fe9ce
vaibhav patel <vaibhav00070@gmail.com>
2024-12-26 21:02:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ async function checkWeather(city){
         }
         
         document.querySelector(".weather").style.display="block"
+        document.querySelector(".error").style.display="none";
         
     }
 }
